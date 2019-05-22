da31d240db3105d0243ccb7066ecf885cdde79bc
gyugyu90 <kh2000park@gmail.com>
2019-05-22 21:55:15 +0900

    12.2.1 Thread 클래스로부터 직접 생성

---
+++ src/ch12/BeepPrintExample2.java
@@ -0,0 +1,21 @@
+package ch12;
+
+public class BeepPrintExample2 {
+    // 매인 스레드와 작업 스레드가 동시에 실행
+    public static void main(String[] args) {
+        Runnable beepTask = new BeepTask();
+        Thread thread = new Thread(beepTask);
+
+        thread.start();
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+}
