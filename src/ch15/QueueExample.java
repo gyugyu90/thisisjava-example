153c9bb130063fac188ca4d650601a2b67096579
gyugyu90 <kh2000park@gmail.com>
2019-05-22 21:26:31 +0900

    15.6.2 Queue

---
+++ src/ch15/QueueExample.java
@@ -0,0 +1,31 @@
+package ch15;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Message> messageQueue = new LinkedList<>();
+        messageQueue.offer(new Message("sendMail", "홍길동"));
+        messageQueue.offer(new Message("sendSMS", "신용권"));
+        messageQueue.offer(new Message("sendKakaotalk", "홍두깨"));
+
+        while(!messageQueue.isEmpty()) {
+            Message message = messageQueue.poll();
+            switch (message.getCommand()) {
+                case "sendMail":
+                    System.out.println(message.getTo() + "님에게 메일을 보냅니다");
+                    break;
+                case "sendSMS":
+                    System.out.println(message.getTo() + "님에게 SMS를 보냅니다");
+                    break;
+                case "sendKakaotalk":
+                    System.out.println(message.getTo() + "님에게 카카오톡을 보냅니다");
+                    break;
+                default:
+                    System.out.println("send nothing");
+                    break;
+            }
+        }
+    }
+}
