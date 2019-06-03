c22fbd38ca9eeb132b0f84a8b1d8fdbbcf5e57a9
gyugyu90 <kh2000park@gmail.com>
2019-06-03 23:25:17 +0900

    13.5 제한된 타입 파라미터(<T extends 최상위타입>)

--- src/ch13/Util.java
+++ src/ch13/Util.java
@@ -14,6 +14,12 @@ public class Util {
         return keyCompare && valueCompare;
     }
 
+    public static <T extends Number> int compare(T t1, T t2) {
+        double v1 = t1.doubleValue();
+        double v2 = t2.doubleValue();
+        return Double.compare(v1, v2);
+    }
+
 }
 
 class Pair<K, V> {
