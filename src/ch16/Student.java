d311df87742ad3cfd2f150dabe71215516502e0e
gyugyu90 <kh2000park@gmail.com>
2019-06-12 12:06:59 +0900

    16.11.1 필터링한 요소 수집

--- src/ch16/Student.java
+++ src/ch16/Student.java
@@ -3,10 +3,33 @@ package ch16;
 public class Student implements Comparable<Student>{
     private String name;
     private int score;
+    private Sex sex;
+    private City city;
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    public enum City {
+        Seoul, Pusan
+    }
 
     public Student(String name, int score) {
         this.name = name;
         this.score = score;
+    }
+
+    public Student(String name, int score, Sex sex) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+    }
+
+    public Student(String name, int score, Sex sex, City city) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+        this.city = city;
     }
 
     public String getName() {
@@ -15,6 +38,14 @@ public class Student implements Comparable<Student>{
 
     public int getScore() {
         return score;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public City getCity() {
+        return city;
     }
 
     @Override
